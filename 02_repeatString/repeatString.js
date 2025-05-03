30b211a46534a6945ce2881f9d85e744b61c96c4
luisevb24 <luisevb.5s@gmail.com>
2025-05-02 22:06:17 -0600

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
-
-// Do not edit below this line
+const repeatString = function(myString, num) {
+    returnString = "";
+    if (num >= 0){
+        for(let i = 0;i < num; i++){
+            returnString = returnString + myString;
+            console.log(returnString);
+        }
+        return returnString;
+    }
+    else {
+        return "ERROR";
+    }
+}
 module.exports = repeatString;
