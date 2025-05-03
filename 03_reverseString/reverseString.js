1a8a0653a9d9b8014e879f98aea2f30e3318e681
luisevb24 <luisevb.5s@gmail.com>
2025-05-02 22:13:06 -0600

    complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    returnString = inputString.split('').reverse().join('');
+    return returnString;
 };
 
 // Do not edit below this line
