b0eb3b1440777333182c927556f5006de8afde15
luisevb24 <luisevb.5s@gmail.com>
2025-05-03 23:28:15 -0600

    complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(
+    inputArray = [], ...args) 
+{       
+        let ogArray = inputArray;
+        let allArgs = args;
+        let toBePopped = [];
+        for(arg of allArgs){
+            let index = ogArray.indexOf(arg, 0);
+            while (index >= 0){
+                toBePopped.push(index);
+                index = ogArray.indexOf(arg, index +1);
+            }
+        }
+        toBePopped.sort((a, b) => b - a);
+
+        for(index of toBePopped){
+            ogArray.splice(index, 1);
+        }
+
+        return ogArray;
 };
 
 // Do not edit below this line
